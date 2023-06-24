49e275e4573c49a7626adf56b035bf885ed97b4d
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-06-24 14:57:00 +0200

    detection of hover and touch

--- helper_classes/touch_input_classes.py
+++ helper_classes/touch_input_classes.py
@@ -1,27 +1,76 @@
 import cv2
+import pyglet
+
+from PIL import Image
+from pyglet import shapes
+
+from collections import deque
 
 # own helper classes
 from helper_classes.agglomerative_clustering_class import Agglomerative_Cluster_Model
 
-class ImageHandler:
+# radius of bounding circles (bbox)
+RADIUS_TOUCH = 50
+RADIUS_HOVER = 30
+# color of bounding circles (bbox)
+COLOR_TOUCH = (255, 0, 0)
+COLOR_HOVER = (0, 128, 0)
+# detection area
+AREA_LOWER_LIMIT = 0
+AREA_UPPER_LIMIT = 50
+# deviation acceptance for new input
+DEVIATION = 50
+
+
+## https://gist.github.com/nkymut/1cb40ea6ae4de0cf9ded7332f1ca0d55
+def cv2glet(img,fmt):
+    '''Assumes image is in BGR color space. Returns a pyimg object'''
+    if fmt == 'GRAY':
+      rows, cols = img.shape
+      channels = 1
+    else:
+      rows, cols, channels = img.shape
+
+    raw_img = Image.fromarray(img).tobytes()
+
+    top_to_bottom_flag = -1
+    bytes_per_row = channels*cols
+    pyimg = pyglet.image.ImageData(width=cols, 
+                                   height=rows, 
+                                   fmt=fmt, 
+                                   data=raw_img, 
+                                   pitch=top_to_bottom_flag*bytes_per_row)
+    return pyimg
+
+class Image_Processor:
     def __init__(self, camera_id):
         self.cap = cv2.VideoCapture(camera_id)
         self.frame = None
         self.thresh = None
-        self.radius_touch = 40
-        self.radus_hover = 60
         self.is_touch = False
         self.is_hover = False
-        self.cutoff_touch = 0
-        self.cutoff_hover = 0
-        self.area_lower_limit = 0
-        self.area_upper_limit = 50
-        self.deviation = 50
+        self.cutoff_touch = 15
+        self.cutoff_hover = 25
         self.aggl_clusterer = Agglomerative_Cluster_Model()
         # dominant fingertip memory
-        self.dom_fing_coordinates:tuple = None
+        self.dom_fing_coordinates:list = deque(maxlen=1)
         # current dominant fingertip touch
-        self.curr_dom_touch = None
+        self.curr_dom_touch:list = deque(maxlen=1)
+
+    def cap_release(self):
+        self.cap.release()
+
+    def set_touch_state(self, touch_variant:str, state:bool):
+        if touch_variant == 'touch':
+            self.is_touch = state
+        elif touch_variant == 'hover':
+            self.is_hover = state
+    
+    def get_touch_state(self):
+        return self.is_touch
+    
+    def get_hover_state(self):
+        return self.is_hover
 
     def get_cutoff_touch(self):
         return self.cutoff_touch
@@ -35,31 +84,19 @@ class ImageHandler:
     def set_hover_cutoff(self, new_cutoff):
         self.cutoff_hover = new_cutoff
 
-    def get_raw_image(self):
-        ret, frame = self.cap.read()
-        
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # transform to grayscale, apply threshold
-        # this allows for pixel-perfect collision detection
-        # another method could be to find the contours and detect collision with the resulting polygon
-        gray_transformed = cv2.cvtColor(frame_transformed, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray_transformed, 160, 255, cv2.THRESH_BINARY_INV)
-
-        self.thresh = thresh
-        out = frame_transformed
-
-        self.frame = out
-
-    def get_pyglet_image(self, touch_variant:str): # touch_variant is "touch" or "hover"
-        cutoff = 0
-        bounding_cirle_radius = 0
+    def process_image(self, touch_variant:str): # touch_variant is "touch" or "hover"
+        #cutoff = 0
+        #bounding_circle_radius = 0
+        #bounding_circle_color = None
         
         if touch_variant == 'touch':
             cutoff = self.cutoff_touch
-            bounding_cirle_radius = self.radius_touch
+            bounding_circle_radius = RADIUS_TOUCH
+            bounding_circle_color = COLOR_TOUCH
         elif touch_variant == 'hover':
             cutoff = self.cutoff_hover
-            bounding_cirle_radius = self.radus_hover
+            bounding_circle_radius = RADIUS_HOVER
+            bounding_circle_color = COLOR_HOVER
         else:
             raise Exception("use 'touch' or 'hover'")
 
@@ -81,7 +118,7 @@ class ImageHandler:
 
         for contour in contours:
             area = cv2.contourArea(contour)
-            if area >= self.area_lower_limit and area <= self.area_upper_limit:
+            if area >= AREA_LOWER_LIMIT and area <= AREA_UPPER_LIMIT:
                 #(x,y),radius = cv2.minEnclosingCircle(contour)
                 #center = (int(x),int(y))
                 #radius = int(radius) * 3
@@ -94,35 +131,57 @@ class ImageHandler:
         
         # Flackern reduzieren
         # old or new position
+        # öö
         if len(area_contours_clustered) > 0:
+            self.set_touch_state(touch_variant, True)
             (x,y),radius = cv2.minEnclosingCircle(area_contours_clustered[0])
-            if self.dom_fing_coordinates == None:
-                self.dom_fing_coordinates = (x,y)
-            if self.curr_dom_touch == None:
-                self.curr_dom_touch = area_contours_clustered[0]
-            x_old, y_old = self.dom_fing_coordinates
-            if x_old - self.deviation < x and x_old + self.deviation> x and y_old - self.deviation < y and y_old + self.deviation > y:
-                area_contours_clustered[0] = self.curr_dom_touch
+            print((x,y))
+            if len(self.dom_fing_coordinates) == 0:
+                self.dom_fing_coordinates.append((x,y))
+            if len(self.curr_dom_touch) == 0:
+                self.curr_dom_touch.append(area_contours_clustered[0])
+            x_old, y_old = self.dom_fing_coordinates[0]
+            if x_old - DEVIATION < x and x_old + DEVIATION > x and y_old - DEVIATION < y and y_old + DEVIATION > y:
+                area_contours_clustered[0] = self.curr_dom_touch[0]
             else:
-                self.dom_fing_coordinates = (x,y)
-                self.curr_dom_touch = area_contours_clustered[0]
-                
+                self.dom_fing_coordinates.append((x,y))
+                self.curr_dom_touch.append(area_contours_clustered[0])
+        else:
+            self.set_touch_state(touch_variant, False)
+            
         for contour in area_contours_clustered:
             area = cv2.contourArea(contour)
             
             (x,y),radius = cv2.minEnclosingCircle(contour)
             center = (int(x),int(y))
-            radius = bounding_cirle_radius
-            cv2.circle(img_bgr,center,radius,(0,255,0),3)
+            radius = bounding_circle_radius
+            cv2.circle(img_bgr,center,radius,bounding_circle_color,3)
             final_areas.append(contour)
     
         img_processed = cv2.drawContours(img_bgr, final_areas, -1, (255, 160, 122), 3)
 
         return img_processed
-
-
-
+    
+    def get_pyglet_image(self):
         img = None
         if self.frame is not None:
             img = cv2glet(self.frame, 'BGR')
         return img 
+    
+
+    '''
+    def get_raw_image(self):
+        ret, frame = self.cap.read()
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # transform to grayscale, apply threshold
+        # this allows for pixel-perfect collision detection
+        # another method could be to find the contours and detect collision with the resulting polygon
+        gray_transformed = cv2.cvtColor(frame_transformed, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray_transformed, 160, 255, cv2.THRESH_BINARY_INV)
+
+        self.thresh = thresh
+        out = frame_transformed
+
+        self.frame = out
+'''
--- touch-input_with_video.py
+++ touch-input_with_video.py
@@ -22,166 +22,40 @@ from pyglet import shapes
 
 # own helper classes
 from helper_classes.agglomerative_clustering_class import Agglomerative_Cluster_Model
+from helper_classes.touch_input_classes import Image_Processor
 
+# for testing purposes with video
 CURR_DIR = path.dirname(__file__)
 #RECORDINGS_PATH = path.join(CURR_DIR, f"assets/{str(uuid.uuid4())}.mp4")
-#RECORDED_SESSION = path.join(CURR_DIR, f"assets/touch_stärker_2.mp4") # Touch
-RECORDED_SESSION = path.join(CURR_DIR, f"assets/touch_hover_leichter_2.mp4") # Hover
+RECORDED_SESSION = path.join(CURR_DIR, f"assets/touch_stärker_2.mp4") # Touch
+#RECORDED_SESSION = path.join(CURR_DIR, f"assets/touch_hover_leichter_2.mp4") # Hover
 
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT, resizable=False)
+image_processor = Image_Processor(RECORDED_SESSION)
 
-# area treshholds for touch/hover detection
-area_lower_limit = 0
-area_upper_limit = 50
-
-# Create a video capture object for the webcam
-cap = cv2.VideoCapture(RECORDED_SESSION)
-
-cutoff = 15 # 15 für touch
-radius_touch = 40
-radus_hover = 60
-
-# dominant fingertip memory
-dom_fing_dq:list = deque(maxlen=1)
-# current dominant fingertip touch
-curr_dom_touch:list = deque(maxlen=1)
-
-aggl_clusterer = Agglomerative_Cluster_Model()
-
-class Game:
-    def __init__(self):
-        self.image_handler = ImageHandler(0)
-        self.enemies = []
-
-    def draw(self):
-        self.image_handler.get_image()
-        img = self.image_handler.get_pyglet_image()
-        if img is not None:
-            img.blit(0,0,0)
-
-## https://gist.github.com/nkymut/1cb40ea6ae4de0cf9ded7332f1ca0d55
-def cv2glet(img,fmt):
-    '''Assumes image is in BGR color space. Returns a pyimg object'''
-    if fmt == 'GRAY':
-      rows, cols = img.shape
-      channels = 1
-    else:
-      rows, cols, channels = img.shape
-
-    raw_img = Image.fromarray(img).tobytes()
-
-    top_to_bottom_flag = -1
-    bytes_per_row = channels*cols
-    pyimg = pyglet.image.ImageData(width=cols, 
-                                   height=rows, 
-                                   fmt=fmt, 
-                                   data=raw_img, 
-                                   pitch=top_to_bottom_flag*bytes_per_row)
-    return pyimg
-
-
-def touch_areas(img_raw):
-    global cutoff, dom_fing_dq, curr_dom_touch, radius_touch
-    
-    # convert the frame to grayscale img for threshold filter and getting the contours of them
-    img_gray = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
-    
-    ret, thresh = cv2.threshold(img_gray, cutoff, 255, cv2.THRESH_BINARY)
-    
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # convert back to colored img to see the touch areas
-    img_bgr = cv2.cvtColor(img_gray, cv2.COLOR_BAYER_BG2BGR)
-
-    # contours of the touched areas
-    touch_areas_contours:list = []
-
-    for contour in contours:
-        area = cv2.contourArea(contour)
-        if area >= area_lower_limit and area <= area_upper_limit:
-            #(x,y),radius = cv2.minEnclosingCircle(contour)
-            #center = (int(x),int(y))
-            #radius = int(radius) * 3
-            #cv2.circle(img_gray,center,radius,(0,255,0),3)q
-            touch_areas_contours.append(contour)
-            
-    area_contours_clustered = aggl_clusterer.agglomerative_clustering(touch_areas_contours)
-        
-    final_areas:list = []
-    
-    #img_contours = cv2.cvtColor(img_gray, cv2.COLOR_BGR2RGB)
-    #img_areas = cv2.drawContours(img_gray, area_contours_clustered, -1, (255, 160, 122), 3)
-    
-    
-    
-        #print(dom_fing_dq[0])
-    '''
-    if len(final_areas) > 0:
-        (x,y),radius = cv2.minEnclosingCircle(final_areas[0])
-        print((x,y))
-        if (int(x), int(y)) in dom_fing_dq:
-            print('old position')
-            final_areas[0] = curr_dom_touch
-        else:
-            dom_fing_dq.append((int(x),int(y)))
-            curr_dom_touch = final_areas[0]
-            #print(dom_fing_dq)
-            print('new')
-    '''
-    
-    # Flackern reduzieren
-    # old or new position
-    if len(area_contours_clustered) > 0:
-        (x,y),radius = cv2.minEnclosingCircle(area_contours_clustered[0])
-        print((x,y))
-        if len(dom_fing_dq) == 0:
-            dom_fing_dq.append((x,y))
-        if len(curr_dom_touch) == 0:
-            curr_dom_touch.append(area_contours_clustered[0])
-        x_old, y_old = dom_fing_dq[0]
-        if x_old - 50 < x and x_old + 50 > x and y_old - 50 < y and y_old + 50 > y:
-            print('old position')
-            area_contours_clustered[0] = curr_dom_touch[0]
-            print(curr_dom_touch)
-        else:
-            dom_fing_dq.append((x,y))
-            curr_dom_touch.append(area_contours_clustered[0])
-            #print(dom_fing_dq)
-            print('new')
-            
-    for contour in area_contours_clustered:
-        area = cv2.contourArea(contour)
-        
-        (x,y),radius = cv2.minEnclosingCircle(contour)
-        center = (int(x),int(y))
-        radius = radius_touch
-        cv2.circle(img_bgr,center,radius,(0,255,0),3)
-        final_areas.append(contour)
-    
-    
-        #print(len(final_areas))
-    img_areas = cv2.drawContours(img_bgr, final_areas, -1, (255, 160, 122), 3)
-
-    return img_areas
 
 while True:
-    # Capture a frame from the webcam
-    ret, frame = cap.read()
+    # check if touch happened
+    processed_img_touch = image_processor.process_image('touch')
+    # check if hover happened
+    processed_img_hover = image_processor.process_image('hover')
 
-    # get touch areas
-    processed_img = touch_areas(frame)
-
-    # Display the frame
-    cv2.imshow('frame', processed_img)
+    if image_processor.get_touch_state():
+        cv2.imshow('frame', processed_img_touch)
+        print("touch")
+    else:
+        if image_processor.get_hover_state():
+            cv2.imshow('frame', processed_img_hover)
+            print("hover")
+        else:
+            cv2.imshow('frame', processed_img_hover)
+            print("none")
 
     # Wait for a key press and check if it's the 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    #time.sleep(0.05)
+    time.sleep(0.05)
 
 # Release the video capture object and close all windows
-cap.release()
+image_processor.cap_release()
 cv2.destroyAllWindows()
