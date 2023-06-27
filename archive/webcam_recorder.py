794c8b1ce3236454572dfd4083984986552b0fbb
Rusi91 <41992838+Rusi91@users.noreply.github.com>
2023-06-27 16:13:10 +0200

    webcam recorder

---
+++ archive/webcam_recorder.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+from os import path
+import uuid
+
+CURR_DIR = path.dirname(__file__)
+RECORDINGS_PATH = path.join(CURR_DIR, f".../assets/gestures/check_hover.mp4")
+
+video_id = 1
+
+if len(sys.argv) > 1:
+    video_id = int(sys.argv[1])
+
+# Create a video capture object for the webcam
+cap = cv2.VideoCapture(video_id)
+
+vid_cod = cv2.VideoWriter_fourcc(*'XVID')
+output = cv2.VideoWriter(RECORDINGS_PATH, vid_cod, 20.0, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+while True:
+    # Capture a frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the frame
+    cv2.imshow('frame', frame)
+    output.write(frame)
+
+    # Wait for a key press and check if it's the 'q' key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close all windows
+cap.release()
+output.release()
+cv2.destroyAllWindows()
